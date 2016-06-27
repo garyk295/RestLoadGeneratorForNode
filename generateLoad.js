feeed79370e21c027b0c4b777a8ddd8096a461ea
Gary <gary.e.kean@gmail.com>
2016-06-27 13:27:30 +0100

    Update generateLoad.js

--- generateLoad.js
+++ generateLoad.js
@@ -1,8 +1,7 @@
 //Description
 //End points are called in sequence (i.e. Only when a response is received is the next request to the REST end point made)
 //
-//Args
-//Argument 1 - How many times to call end points
+
 
 console.log("Starting to call REST Services");
 
@@ -10,9 +9,10 @@ console.log("Starting to call REST Services");
 var querystring = require('querystring');
 var https = require('https');
 var loopTimes = process.argv[2];
+var timeBetweenCalls = process.argv[3];
 var count = 0;
 
-//Read config file
+//Read configuration file
 var fs, configurationFile;
 configurationFile = 'configuration.json';
 fs = require('fs');
@@ -21,8 +21,9 @@ var configuration = JSON.parse(
 );
 
 
-//Call Perform Request function
-performRequest(configuration[0].host, configuration[0].path,'GET', '', configuration[0].clientId, configuration[0].clientSecret, configuration[0].methodName, function(data) {
+//Call Perform Request function starting with the first end point in the configuration file
+performRequest(configuration[0].host, configuration[0].path, configuration[0].method, configuration[0].data, configuration[0].clientId, configuration[0].clientSecret, configuration[0].methodName, function(data) {
+console.log(data);
 });
 
 //Random Integer function
@@ -34,13 +35,12 @@ function getRandomInt(min, max) {
 
 
 //Perform Request Function
- function performRequest(fHost, endpoint, method, data, fClientId, fClientSecret, methodName, success) {
+ function performRequest(fHost, endpoint, method, requestData, fClientId, fClientSecret, methodName, result) {
    count = count +1;
    loopTimes = loopTimes - 1;
-   console.log("Invocation: " + count);
-   console.log("Method: " + methodName);
+   console.log("Invocation: " + count + " |  Method: " + methodName);
 
-   var dataString = JSON.stringify(data);
+   var dataString = JSON.stringify(requestData);
    var headers = {};
 
    headers =
@@ -59,32 +59,64 @@ function getRandomInt(min, max) {
    };
 
    var req = https.request(options, function(res) {
-     res.setEncoding('utf-8');
 
-     var responseString = '';
+       res.setEncoding('utf-8');
 
-     res.on('data', function(data) {
-       responseString += data;
-     });
+       var responseString = '';
 
-     res.on('end', function() {
-       //console.log(responseString);
-       var responseObject = JSON.parse(responseString);
-       success(responseObject);
-       if(loopTimes > 0)
-       {
-         var randomInteger = getRandomInt(0, configuration.length);
-         //Repeat call to Perform Request function
-         performRequest(configuration[randomInteger].host, configuration[randomInteger].path, configuration[randomInteger].method, '', configuration[randomInteger].clientId, configuration[randomInteger].clientSecret, configuration[randomInteger].methodName, function(data) {
-         });
-       }
-       else
-       {
-         console.log("Script Complete");
-       }
-     });
+       res.on('data', function(data) {
+         responseString += data;
+
+       });
+
+       res.on('end', function() {
+
+           if(res.statusCode != 200)
+           {
+             result("Error: " + res.statusCode + " | " + res.statusMessage);
+             //console.log(res);
+           }
+           else
+           {
+             result("Success: " + res.statusCode + " | " + res.statusMessage);
+             //Don't need to do anything with the response object therefore the below code is commented out
+             //var responseObject = JSON.parse(responseString);
+           }
+
+
+           if(loopTimes > 0)
+           {
+             var randomInteger = getRandomInt(0, configuration.length);
+             //Repeat call to Perform Request function
+             setTimeout(function() {
+               performRequest(configuration[randomInteger].host, configuration[randomInteger].path, configuration[randomInteger].method, configuration[randomInteger].data, configuration[randomInteger].clientId, configuration[randomInteger].clientSecret, configuration[randomInteger].methodName, function(data) {
+                 console.log(data);
+               });
+             }, timeBetweenCalls
+            );
+           }
+           else
+           {
+             console.log("Script Complete");
+           }
+       });
+
+
+
    });
 
    req.write(dataString);
    req.end();
+   req.on('error', function(e) {
+     console.log("Error: " + e);
+
+     var randomInteger = getRandomInt(0, configuration.length);
+     //Repeat call to Perform Request function
+     setTimeout(function() {
+       performRequest(configuration[randomInteger].host, configuration[randomInteger].path, configuration[randomInteger].method, configuration[randomInteger].data, configuration[randomInteger].clientId, configuration[randomInteger].clientSecret, configuration[randomInteger].methodName, function(data) {
+         console.log(data);
+       });
+     }, timeBetweenCalls
+    );
+   });
  }
